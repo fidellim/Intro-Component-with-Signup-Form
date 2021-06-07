7206043e5731383e5108c41b00bf8f3085b862bb
fidellim <lim.fidel@gmail.com>
2021-06-07 08:38:57 +0400

    add form validation

---
+++ script.js
@@ -0,0 +1,65 @@
+const formSubmit = document.querySelector(".form-submit");
+const firstNameInput = document.querySelector(".firstNameInput");
+const lastNameInput = document.querySelector(".lastNameInput");
+const emailInput = document.querySelector(".emailInput");
+const passwordInput = document.querySelector(".passwordInput");
+const firstNameErrorIcon = document.querySelector(".firstNameErrorIcon");
+const firstNameErrorText = document.querySelector(".firstNameErrorText");
+const lastNameErrorIcon = document.querySelector(".lastNameErrorIcon");
+const lastNameErrorText = document.querySelector(".lastNameErrorText");
+const emailErrorIcon = document.querySelector(".emailErrorIcon");
+const emailErrorText = document.querySelector(".emailErrorText");
+const passwordErrorIcon = document.querySelector(".passwordErrorIcon");
+const passwordErrorText = document.querySelector(".passwordErrorText");
+
+const regex = /^[A-Za-z0-9]+@[a-z]{3,}.[a-z]{3,}$/;
+
+formSubmit.addEventListener("submit", (e) => {
+	e.preventDefault(); //prevent refresh of page
+
+	if (firstNameInput.value.trim() === "") {
+		firstNameErrorIcon.style.visibility = "visible";
+		firstNameErrorText.style.display = "block";
+		firstNameInput.style.border = "2px solid rgb(255, 122, 122)";
+	} else {
+		firstNameErrorIcon.style.visibility = "hidden";
+		firstNameErrorText.style.display = "none";
+		firstNameInput.style.border = "1px solid rgb(185, 182, 211)";
+	}
+
+	if (lastNameInput.value.trim() === "") {
+		lastNameErrorIcon.style.visibility = "visible";
+		lastNameErrorText.style.display = "block";
+		lastNameInput.style.border = "2px solid rgb(255, 122, 122)";
+	} else {
+		lastNameErrorIcon.style.visibility = "hidden";
+		lastNameErrorText.style.display = "none";
+		lastNameInput.style.border = "1px solid rgb(185, 182, 211)";
+	}
+
+	if (emailInput.value.trim() === "") {
+		emailErrorIcon.style.visibility = "visible";
+		emailErrorText.style.display = "block";
+		emailErrorText.innerHTML = "Email cannot be empty";
+		emailInput.style.border = "2px solid rgb(255, 122, 122)";
+	} else if (!regex.test(emailInput.value.trim())) {
+		emailErrorIcon.style.visibility = "visible";
+		emailErrorText.style.display = "block";
+		emailErrorText.innerHTML = "Looks like this is not an email";
+		emailInput.style.border = "2px solid rgb(255, 122, 122)";
+	} else {
+		emailErrorIcon.style.visibility = "hidden";
+		emailErrorText.style.display = "none";
+		emailInput.style.border = "1px solid rgb(185, 182, 211)";
+	}
+
+	if (passwordInput.value === "") {
+		passwordErrorIcon.style.visibility = "visible";
+		passwordErrorText.style.display = "block";
+		passwordInput.style.border = "2px solid rgb(255, 122, 122)";
+	} else {
+		passwordErrorIcon.style.visibility = "hidden";
+		passwordErrorText.style.display = "none";
+		passwordInput.style.border = "1px solid rgb(185, 182, 211)";
+	}
+});
